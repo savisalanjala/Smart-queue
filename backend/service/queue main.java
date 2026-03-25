67f6b3ab9a754a44e0339eab3b9d8c9825cdb8f9
savisalanjala <saviezhilarasan17@gmail.com>
2026-03-25 08:53:39 +0530

    Create queue main.java

---
+++ backend/service/queue main.java
@@ -0,0 +1,13 @@
+package com.example.queueueueeeee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueueueeeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueueueeeeeApplication.class, args);
+	}
+
+}
