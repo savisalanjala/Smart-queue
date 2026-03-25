e02b4d2c5c6b1071a148e5db44e514f29cb08c67
savisalanjala <saviezhilarasan17@gmail.com>
2026-03-25 08:47:32 +0530

    Create Admin entity with fields and relationships

---
+++ backend/model/admin.java
@@ -0,0 +1,31 @@
+package com.example.queueueueeeee.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "place_id")
+    @JsonManagedReference
+    private Place place;
+
+}
+
+
+
